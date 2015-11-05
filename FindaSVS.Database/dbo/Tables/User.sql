732976e8569eb84e057233cb40f63622446966e9
Wei Bao <Wei.Bao@gov.ab.ca>
2015-11-04 17:30:20 -0700

    Project Setup

---
+++ FindaSVS.Database/dbo/Tables/User.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserId] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(100) NOT NULL, 
+    [LastName] NVARCHAR(100) NOT NULL, 
+    [Email] NVARCHAR(100) NOT NULL, 
+	[Password] NVARCHAR(100) NOT NULL,
+    [Address] NVARCHAR(200) NULL, 
+    [PostalCode] NVARCHAR(50) NULL, 
+    [City] NVARCHAR(50) NULL, 
+    [Country] NVARCHAR(50) NULL
+)
---
+++ FindaSVS.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FindaSVS.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Govt of Alberta")]
+[assembly: AssemblyProduct("FindaSVS.Web")]
+[assembly: AssemblyCopyright("Copyright © Govt of Alberta 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("87f4013c-d3bc-48e0-966f-330719a2f268")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
